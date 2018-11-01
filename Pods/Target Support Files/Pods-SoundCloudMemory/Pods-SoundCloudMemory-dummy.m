7d15ef910e0fa21b22c5f45158476dac9648d0a1
Gokhan Akkurt <akkurtgokhan@gmail.com>
2018-11-02 01:07:00 +0300

    updated gitignore.

--- Pods/Target Support Files/Pods-SoundCloudMemory/Pods-SoundCloudMemory-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SoundCloudMemory : NSObject
-@end
-@implementation PodsDummy_Pods_SoundCloudMemory
-@end
--- Pods/Target Support Files/Pods-SoundCloudMemoryTests/Pods-SoundCloudMemoryTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SoundCloudMemoryTests : NSObject
-@end
-@implementation PodsDummy_Pods_SoundCloudMemoryTests
-@end
